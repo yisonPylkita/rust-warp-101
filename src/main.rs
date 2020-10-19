e741023799b06f5bd24bce272bec10d8507d3825
Wojciech Bartnik <yisonPylkita@gmail.com>
2020-10-19 14:18:17 +0000

    Changed greet message and added doc's comment

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,9 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    // Use localhost:8080/hello/Adam
     let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello {}", name));
+        .map(|name| format!("Nice to meet you - {}", name));
 
     warp::serve(hello)
         .run(([127, 0, 0, 1], 8080))
